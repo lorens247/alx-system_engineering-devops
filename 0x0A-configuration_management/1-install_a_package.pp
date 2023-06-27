4416f919dd238eb9d9a503c3fbf8fc22317d1ef6
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-06-27 13:25:43 -0700

    config mgmt updated

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,16 +1,6 @@
 # Install flask from pip3
 
-exec { 'install_flask':
-  command     => '/usr/bin/pip3 install flask==2.1.0',
-  path        => ['/usr/bin'],
-  creates     => '/usr/local/lib/python3.8/dist-packages/flask',
-  require     => Package['python3-pip'],
-  notify      => Exec['check_flask_version'],
-}
-
-exec { 'check_flask_version':
-  command     => 'flask --version',
-  path        => ['/usr/bin'],
-  logoutput   => true,
-  refreshonly => true,
+package { 'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3',
 }
--- 0x0A-configuration_management/2-execute_a_command.pp
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -1,6 +1,6 @@
 # Create a manifest that kills a process named killmenow
 
 exec { 'killmenow':
-  command => 'pkill -f killmenow',
-  onlyif  => 'pgrep -f killmenow',
+  command  => 'pkill -f killmenow',
+  provider => 'shell',
 }
