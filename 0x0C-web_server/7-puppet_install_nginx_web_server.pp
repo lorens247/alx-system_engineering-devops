861c50cf128c0a03c29dd05da086a89a543e8e10
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-07-07 15:35:29 -0700

    fix

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,53 +1,21 @@
-# Configuring your server with Puppet!
+# puppet: install and configure Nginx
 
-# Install Nginx package
 package { 'nginx':
   ensure => installed,
 }
- 
-# Configure Nginx server
-file { '/etc/nginx/sites-available/default':
-  ensure  => file,
-  content => "
-server {
-    listen 80 default_server;
-    server_name _;
-    root /var/www/html;
 
-    location / {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
-    }
-
-    location / {
-        index index.html;
-    }
-}
-",
+file_line { 'server_config':
+  ensure => 'present',
+  path   => '/etc/nginx/sites-available/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^/redirect_me https://www.youtube.com/watch?v=QH2-TGUlwu4/ permanent;',
 }
 
-# Enable the default site by creating a symbolic link using shell provider
-exec { 'enable_nginx_default_site':
-  command     => 'ln -sf /etc/nginx/sites-available/default /etc/nginx/sites-enabled/default',
-  path        => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-  refreshonly => true,
-  require     => File['/etc/nginx/sites-available/default'],
-}
-
-# Remove default Nginx welcome page
 file { '/var/www/html/index.html':
-  ensure => absent,
+  content => 'Hello World!',
 }
 
-# Ensure Nginx service is running and enabled
 service { 'nginx':
   ensure  => running,
-  enable  => true,
-  hasstatus => true,
-  provider => 'shell',
-  status   => '/bin/systemctl status nginx | grep -q "active (running)"',
-  require  => Package['nginx'],
+  require => Package['nginx'],
 }
