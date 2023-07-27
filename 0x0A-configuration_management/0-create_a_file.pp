f4c9bb6353b8540891d63b73532239b361960382
lorens247 <Lawrence Ikhidero>
2023-07-27 03:00:21 +0100

    roll back

--- 0x0A-configuration_management/0-create_a_file.pp
+++
@@ -1,9 +0,0 @@
-# Create a file in /tmp called 'school'
-
-file { '/tmp/school':
-  ensure  => file,
-  mode    => '0744',      # Set file permissions to 0744
-  owner   => 'www-data',  # Set file owner to www-data
-  group   => 'www-data',  # Set file group to www-data
-  content => 'I love Puppet',  # Set file content to 'I love Puppet'
-}
--- 0x0A-configuration_management/2-execute_a_command.pp
+++
@@ -1,6 +0,0 @@
-# Create a manifest that kills a process named killmenow
-
-exec { 'killmenow':
-  command  => 'pkill -f killmenow',
-  provider => 'shell',
-}
