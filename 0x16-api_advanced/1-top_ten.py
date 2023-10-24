cfea694867e54b7a26a9103a2e6e669bd66bf124
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-10-24 09:01:18 -0700

    top ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -8,15 +8,15 @@ import requests
 def top_ten(subreddit):
     # URL for Reddit API endpoint to get hot posts in a subreddit
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
-    
+
     # Make a GET request to the Reddit API
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
     # Check if the response is successful (status code 200)
     if response.status_code == 200:
-        
+
         # Parse the JSON response
         data = response.json()
-        
+
         # Extract and print titles of the first 10 hot posts
         for post in data['data']['children']:
             print(post['data']['title'])
@@ -32,4 +32,6 @@ def top_ten(subreddit):
             else:
                 subreddit = sys.argv[1]
                 top_ten(subreddit)
+
+
 
