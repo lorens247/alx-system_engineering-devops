9c7c50e2833f48bffe89e2e90ff6b2391673af07
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-10-24 08:46:40 -0700

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+A function that queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+'''
+
+import requests
+def top_ten(subreddit):
+    # URL for Reddit API endpoint to get hot posts in a subreddit
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    
+    # Make a GET request to the Reddit API
+    response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
+    # Check if the response is successful (status code 200)
+    if response.status_code == 200:
+        
+        # Parse the JSON response
+        data = response.json()
+        
+        # Extract and print titles of the first 10 hot posts
+        for post in data['data']['children']:
+            print(post['data']['title'])
+    else:
+        # If the subreddit is not valid, print None
+        print(None)
+
+        # This block allows the script to be run from the command line
+        if __name__ == '__main__':
+            import sys
+            if len(sys.argv) < 2:
+                print("Please pass an argument for the subreddit to search.")
+            else:
+                subreddit = sys.argv[1]
+                top_ten(subreddit)
+
