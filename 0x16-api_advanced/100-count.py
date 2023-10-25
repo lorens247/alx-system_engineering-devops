20764db2b6b361393f7b76e43b6bdd6f1f277c14
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-10-25 04:24:04 -0700

    count it!

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -6,6 +6,7 @@ import requests
 
 
 def count_words(subreddit, word_list, after=None, word_counts=None):
+    
     # Base URL for Reddit API endpoint to get hot posts in a subreddit
     url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=100"
     
@@ -13,20 +14,20 @@ def count_words(subreddit, word_list, after=None, word_counts=None):
     if after:
         url += f"&after={after}"
 
-    # Make a GET request to the Reddit API
+   # Make a GET request to the Reddit API
     response = requests.get(url, headers={"User-Agent": "Mozilla/5.0"})
-    
+
     # Check if the response is successful (status code 200)
     if response.status_code == 200:
         # Parse the JSON response
         data = response.json()
-        
+
         # Initialize word_counts dictionary if it's None
         if word_counts is None:
             word_counts = {}
             for word in word_list:
                 word_counts[word.lower()] = 0
-        
+
         # Extract titles of posts and count occurrences of keywords
         for post in data['data']['children']:
             title = post['data']['title'].lower()
@@ -34,12 +35,15 @@ def count_words(subreddit, word_list, after=None, word_counts=None):
                 if word.lower() in title:
                     word_counts[word.lower()] += 1
         
-        # If there are more posts, recursively call the function with the 'after' parameter
+        # If there are more posts, recursively call the function
+        # with the 'after' parameter
         if data['data']['after']:
-            return count_words(subreddit, word_list, after=data['data']['after'], word_counts=word_counts)
+            return count_words(subreddit, word_list,
+                    after=data['data']['after'], word_counts=word_counts)
         else:
-            # If no more posts, print word counts in descending order by count and ascending order alphabetically
-            sorted_counts = sorted(word_counts.items(), key=lambda x: (-x[1], x[0]))
+            # If no more posts, print word counts in descending order
+            sorted_counts = sorted(word_counts.items(),
+                    key=lambda x: (-x[1], x[0]))
             for word, count in sorted_counts:
                 print(f"{word}: {count}")
     else:
@@ -56,5 +60,3 @@ if __name__ == '__main__':
         subreddit = sys.argv[1]
         keywords = sys.argv[2:]
         count_words(subreddit, keywords)
-
-
