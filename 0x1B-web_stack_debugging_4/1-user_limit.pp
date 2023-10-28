c372b406a6daee11f59c5b452f98d62e308c317b
Lawrence Ikhidero <lawrence@ikhidero.com>
2023-10-28 07:12:44 -0700

    updated web ebugging

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,15 +1,15 @@
-# Change the OS configuration to login with the holberton user;
+# Change the OS configuration to login with the holberton user
 # and open a file without any error message.
 
 exec { 'correct-hard':
-	command  => 'sudo sed -i \'s/nofile 5/nofile 30000/\' /etc/security/limits.conf',
-	provider => shell,
+command  => 'sudo sed -i \'s/nofile 5/nofile 30000/\' /etc/security/limits.conf',
+provider => shell,
 }
-	exec { 'correct-soft':
-	command  => 'sudo sed -i \'s/nofile 4/nofile 10000/\' /etc/security/limits.conf',
-	provider => shell,
+exec { 'correct-soft':
+command  => 'sudo sed -i \'s/nofile 4/nofile 10000/\' /etc/security/limits.conf',
+provider => shell,
 }
 exec { 'change-os-configuration-for-holberton-user':
-	command  => 'sysctl -p',
-	provider => shell,
+command  => 'sysctl -p',
+provider => shell,
 } 
